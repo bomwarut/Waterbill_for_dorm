5922a2735f0d262bf47dc10a23ba4925ec98ab21
bomwarut <89127532+bomwarut@users.noreply.github.com>
2022-12-04 23:14:01 +0700

    first commit

---
+++ findBill.ts
@@ -0,0 +1,18 @@
+
+var eBill_val : string = process.argv[2];
+var eBill : number = Number(eBill_val);
+
+// var choice : string = process.argv[2];
+// if (choice == "1"){
+//     console.log("1");
+// }else if (choice == "2"){
+//     console.log("2");
+// }else if (choice == "3"){
+//     console.log("3");
+// }else if (choice == "4"){
+//     console.log("4");
+// }else if (choice == "5"){
+//     console.log("5");
+// }else{
+//     console.log("Error please enter the number of choice");
+// }
